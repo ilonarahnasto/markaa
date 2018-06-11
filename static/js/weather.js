228eb140e14d5ccea169878bcd18d224c05a8959
Ilona Rahnasto <ilona.rahnasto@gmail.com>
2018-06-11 17:26:37 +0300

    voisitkopliispliis

---
+++ static/js/weather.js
@@ -0,0 +1,16 @@
+function addPercentage() {
+  var parameters = {
+    "url": "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/133328?apikey=ERXjpvLv9nStpPKFXauhfWUWc8JfD7Av&language=fi",
+    "method": "GET",
+    "headers": {}
+  }
+
+  $.ajax(parameters).done(function (data) {
+    var percentage = data[0].PrecipitationProbability;
+    var node = document.createElement("H3");
+    var textnode = document.createTextNode("Tällä hetekllä vituttaa " + percentage + " prosenttisesti.");
+    node.appendChild(textnode);   
+    document.getElementById("vituttaa").appendChild(node);
+  });
+
+}
